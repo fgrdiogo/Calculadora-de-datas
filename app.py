cb6181050396e3e6f243f43b3691b0ebbacb0ee5
fgrdiogo <diogogomesf5@gmail.com>
2025-12-27 16:44:44 -0300

    adicionado a função de escolher qual unidade de tempo o usuário deseja usar

--- app.py
+++ app.py
@@ -8,22 +8,79 @@ def homepage():
     resultado = None
 
     if request.method == 'POST':
-        semanas = int(request.form.get('semanas_input'))
+        tipo = request.form.get('tipo')
 
-        operacao = request.form.get('operacao')
+        if tipo == 'semana':    
+            semanas = int(request.form.get('semanas_input'))
 
-        if operacao == '(-)':
-            semanas_digitadas = timedelta(weeks=semanas)
-            data_atual = datetime.now()
-            data_antiga = data_atual - semanas_digitadas
-            data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
-            resultado = data_antiga_formatada
-        else:
-            semanas_digitadas = timedelta(weeks=semanas)
-            data_atual = datetime.now()
-            data_antiga = data_atual + semanas_digitadas
-            data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
-            resultado = data_antiga_formatada
+            operacao = request.form.get('operacao')
+
+            if operacao == '(-)':
+                semanas_digitadas = timedelta(weeks=semanas)
+                data_atual = datetime.now()
+                data_antiga = data_atual - semanas_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+            else:
+                semanas_digitadas = timedelta(weeks=semanas)
+                data_atual = datetime.now()
+                data_antiga = data_atual + semanas_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+
+        elif tipo == 'dia':
+            dias = int(request.form.get('semanas_input'))
+
+            operacao = request.form.get('operacao')
+
+            if operacao == '(-)':
+                dias_digitadas = timedelta(days=dias)
+                data_atual = datetime.now()
+                data_antiga = data_atual - dias_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+            else:
+                dias_digitadas = timedelta(days=dias)
+                data_atual = datetime.now()
+                data_antiga = data_atual + dias_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+
+        elif tipo == 'mes':
+            mes = int(request.form.get('semanas_input'))
+
+            operacao = request.form.get('operacao')
+
+            if operacao == '(-)':
+                mes_digitado = mes * timedelta(days=30)
+                data_atual = datetime.now()
+                data_antiga = data_atual - mes_digitado
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+            else:
+                mes_digitado = ano * timedelta(days=365)
+                data_atual = datetime.now()
+                data_antiga = data_atual + mes_digitado
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+        
+        elif tipo == 'ano':
+            ano = int(request.form.get('semanas_input'))
+
+            operacao = request.form.get('operacao')
+
+            if operacao == '(-)':
+                ano_digitadas = ano * timedelta(days=365)
+                data_atual = datetime.now()
+                data_antiga = data_atual - ano_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
+            else:
+                ano_digitadas = ano * timedelta(days=365)
+                data_atual = datetime.now()
+                data_antiga = data_atual + ano_digitadas
+                data_antiga_formatada = data_antiga.strftime("%d/%m/%y")
+                resultado = data_antiga_formatada
     
     return render_template("index.html", data_final=resultado)
 
